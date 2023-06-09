6fb270a99eae6c4874200dd9be31631cd8e28927
Rakesh Paul <42863694+rakeshpaul99@users.noreply.github.com>
2023-06-09 16:45:10 +0530

    add yml file support

--- validate-ocp-properties.sh
+++ validate-ocp-properties.sh
@@ -11,7 +11,6 @@ FILE_NAME=$6
 PROPERTY_KEY=$7
 PROPERTY_VAL=$8
 
-# OCP login
 oc login $OCP_CLUSTER -u $OCP_LOGIN_USER -p $OCP_LOGIN_PASS > /dev/null
 oc project $OCP_PROJECT > /dev/null
 
@@ -20,6 +19,12 @@ FILE_EXTENSION=`echo $FILE_NAME | rev | cut -d'.' -f1 | rev`
 
 if [[ $FILE_EXTENSION == "properties" ]]; then
   if [[ $(oc get cm $OCP_CONFIG_MAPS -o json | jq ".data.\"$FILE_NAME\"" | sed 's/\\\\r\\\\n *\\\\\\n\(\\t\)*//g' | sed 's/\\\\r\\\\n//g' | sed 's/^"//' | sed 's/"$//' | awk '{gsub(/\\n/,"\n")}1'  | sed 's/ *= */=/g' | grep -xc ^$PROPERTY_KEY=$PROPERTY_VAL$) -gt 0 ]]; then
+    echo "Property matched for $OCP_PROJECT:$FILE_NAME!!\n\033[0;32m$PROPERTY_KEY=$PROPERTY_VAL\033[0m"
+  else
+    echo "Property not found in $OCP_PROJECT:$FILE_NAME!!\n\033[0;31m$PROPERTY_KEY=\033[0m"
+  fi
+elif [[ $FILE_EXTENSION == "yml" ]]; then
+  if [[ $(oc get cm $OCP_CONFIG_MAPS -o json | jq ".data.\"$FILE_NAME\"" -r | yq -N ".$PROPERTY_KEY" | grep -xv null | grep -xc $PROPERTY_VAL) -gt 0 ]]; then
     echo "Property matched for $OCP_PROJECT:$FILE_NAME!!\n\033[0;32m$PROPERTY_KEY=$PROPERTY_VAL\033[0m"
   else
     echo "Property not found in $OCP_PROJECT:$FILE_NAME!!\n\033[0;31m$PROPERTY_KEY=\033[0m"
